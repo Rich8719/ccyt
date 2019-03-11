6327d2546439c9260b80b2f0fa262f44dc5887a8
Rich Johnson <r.johnson8719@gmail.com>
2019-03-11 18:04:04 -0400

    removed form component to App.

---
+++ src/components/Captions.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const captionLog = captions => {
+  console.log("yay", captions)
+}
+
+// const Captions = props => {
+//   return <p>Hello</p>
+// }
+
+export default captionLog
