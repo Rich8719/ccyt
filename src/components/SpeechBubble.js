402b06fed9046289c9f3c376c2a304b05ef0599f
Rich Johnson <r.johnson8719@gmail.com>
2019-05-22 17:02:37 -0400

    Speech bubble component and styling

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -4,15 +4,28 @@ class SpeechBubble extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      positionX: Number,
-      positionY: Number
+      posX: Number,
+      posY: Number,
+      bubbleStyle: "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/speech-bubble.svg')"
     }
   }
 
+  getSpeechBubble = () => {
+    // gets the correct speech bubble based on face position (posX and posY)
+    // setsState of bubble style
+    // or just creates a component with svg in it
+  }
+  
   render() {
+    let bubbleStyle = {
+      backgroundImage: this.state.bubbleStyle
+    }
     return (
-      <div>
-        <div className="captions">{this.props.word}</div>
+      <div className="speech-wrapper">
+        <div className="bubble" style={bubbleStyle}>
+          <div className="speech">{this.props.word}
+          </div>
+        </div>
         <div className="sound-effects">{this.props.soundEffect}</div>
       </div>
     )
